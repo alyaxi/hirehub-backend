4c98d6d2fb778384aa212d05a5da8c51fff25eab
ali zaman <ali.zaman@bt.pk>
2024-01-26 22:12:04 +0500

    Update manage jobs

--- routes/Admin/AdminRoutes.js
+++ routes/Admin/AdminRoutes.js
@@ -5,6 +5,7 @@ const checkRole = require("../../middleware/CheckRole");
 const { getAllEmployersWithPayments, changeStatusEmployer, deleteEmployerByID, ViewEmployerById } = require("../../controllers/Admin/ManageEmployers");
 const { getAllCandidateAppliedJobs, changeStatusAppliedJobAdmin } = require("../../controllers/Admin/ManageCandidate");
 const { validateAppliedJob } = require("../../utilis/BodyValidator");
+const { getJobsByAll } = require("../../controllers/Employer/JobPosting");
 
 router.get('/admin/manage-employers', authMiddleware ,  checkRole(['admin']), getAllEmployersWithPayments);
 router.post('/admin/change-status-employers', authMiddleware ,  checkRole(['admin']), changeStatusEmployer);
@@ -12,6 +13,8 @@ router.post('/admin/delete-employers', authMiddleware ,  checkRole(['admin']), d
 router.get('/admin/view-employer-by-id', authMiddleware ,  checkRole(['admin']), ViewEmployerById);
 router.get('/admin/manage-candidates', authMiddleware ,  checkRole(['admin']), getAllCandidateAppliedJobs);
 router.post('/admin/change-manage-candidates-status', authMiddleware ,  checkRole(['admin']),validateAppliedJob, changeStatusAppliedJobAdmin);
+router.get("/admin/get-jobs-by-all", authMiddleware,checkRole(['admin']), getJobsByAll)
+
 
 
 module.exports = router;
