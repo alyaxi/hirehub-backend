b7dba720d2184bbbc1036d4e2bcaa1d6735e3cc2
ali zaman <ali.zaman@bt.pk>
2024-02-02 22:36:50 +0500

    update candidate profile

--- controllers/Candidates/Candidate.js
+++ controllers/Candidates/Candidate.js
@@ -27,19 +27,55 @@ const ManageCandidateProfile = {
     async updateCandidateInfo(req, res, next) {
         try {
             const candidateId = req.user.id;
+            const protocol = req.protocol;
+            const host = req.get('host');
 
+            let link;
+
+            if (host === 'localhost:4000' && protocol === 'http') {
+                link = 'http://localhost:4000';
+            } else if (host === '167.99.148.81' && protocol === 'http') {
+                link = 'http://167.99.148.81/server';
+            } else {
+                // Default link if conditions are not met
+                link = 'default-link';
+            }
+
+            // Process file uploads
+            const profilePicture = req.files["profilePicture"] ? `${link}/${req.files['profilePicture'][0].originalname}` : '';
+            const introVideo = req.files["introVideo"] ? `${req?.protocol}://${req?.get("host")}/${req?.files['introVideo'][0]?.originalname}` : '';
+            const projectImage = req.files["projectImage"] ? `${req?.protocol}://${req?.get("host")}/${req?.files['projectImage'][0]?.originalname}` : '';
+
+            // Find existing user data
             let existingUserData = await CandidateProfile.findOne({ userId: candidateId });
 
             if (!existingUserData) {
                 return respond(res, { error: "Candidate profile not found" }, 404);
             }
 
-            const updatedUserData = { ...existingUserData.toObject(), ...req.body };
+            // Merge existing data with new data
+            const updatedUserData = {
+                ...existingUserData ? existingUserData.toObject() : {},
+                ...req.body,
+                personalInformationData: {
+                    ...existingUserData?.personalInformationData ? existingUserData.personalInformationData.toObject() : {},
+                    ...req.body?.personalInformationData,
+                    profilePicture: profilePicture || (existingUserData?.personalInformationData?.profilePicture || ''),
+                },
+                introVideo: introVideo || existingUserData.introVideo,
+                projectsData: req.body.projectsData.map(project => ({
+                    ...existingUserData.projectsData.id(project.id) ? existingUserData.projectsData.id(project.id).toObject() : {},
+                    ...project,
+                    projectImage: projectImage || existingUserData.projectsData.id(project.id)?.projectImage,
+                })),
+                
+            };
 
+            // Update candidate profile
             const updatedProfile = await CandidateProfile.findOneAndUpdate(
                 { userId: candidateId },
                 updatedUserData,
-                { new: true } 
+                { new: true }
             );
 
             if (!updatedProfile) {
@@ -51,7 +87,7 @@ const ManageCandidateProfile = {
             console.error(error);
             next(error);
         }
-    },
+    }
 }
 
-    module.exports = ManageCandidateProfile;
+module.exports = ManageCandidateProfile;
--- routes/Candidates/candidate-routes.js
+++ routes/Candidates/candidate-routes.js
@@ -5,13 +5,16 @@ const checkRole = require("../../middleware/CheckRole");
 const { updateCandidateInfo, getCandidate } = require("../../controllers/Candidates/Candidate");
 const { createCandidateProfileValidator } = require("../../utilis/BodyValidator");
 const router = express.Router();
+const multer = require('multer');
+const multerConfig = require("../../middleware/multerConfig");
 
 
-
+const upload = multer(multerConfig)
+const cpUpload = upload.fields([{ name: 'profilePicture', maxCount: 1 }, { name: 'introVideo', maxCount: 1 }, { name: 'projectImage', maxCount: 1 }])
 
 
 router.get("/get-candidate", authMiddleware,checkRole(['candidate']), getCandidate)
-router.post("/update-candidate", authMiddleware, checkRole(['candidate']),createCandidateProfileValidator, updateCandidateInfo)
+router.post("/update-candidate", authMiddleware, checkRole(['candidate']),createCandidateProfileValidator,cpUpload, updateCandidateInfo)
 // router.post("/update-jobs-by-employer/:jobId", authMiddleware, checkRole(['candidate']), updateJobByEmployer)
 // router.post("/delete-jobs-by-employer/:jobId", authMiddleware, checkRole(['candidate']), deleteJobByEmployer)
 
