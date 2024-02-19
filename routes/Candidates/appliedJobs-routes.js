0a6f546d1cd71327bd9b94217e83a1df514bb685
ali zaman <ali.zaman@bt.pk>
2024-02-19 15:14:02 +0500

    updated

--- routes/Candidates/appliedJobs-routes.js
+++ routes/Candidates/appliedJobs-routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const authMiddleware = require("../../middleware/authMiddleware");
 const checkRole = require("../../middleware/CheckRole");
 const AppliedJobsCandidate = require("../../controllers/Candidates/AppliedJob");
+const { getJobsByAll } = require("../../controllers/Employer/JobPosting");
 const router = express.Router();
 
 
@@ -12,6 +13,8 @@ const router = express.Router();
 router.get("/get-applied-jobs", authMiddleware,checkRole(['candidate']), AppliedJobsCandidate.getAppliedJobsByCandidate)
 router.get("/get-applied-job-details", authMiddleware,checkRole(['candidate']), AppliedJobsCandidate.getAppliedJobsDetail)
 router.post("/apply-for-job/:jobId", authMiddleware,checkRole(['candidate']), AppliedJobsCandidate.applyForJob)
+router.get("/get-all-jobs", authMiddleware,checkRole(['candidate']), getJobsByAll)
+
 
 
 
