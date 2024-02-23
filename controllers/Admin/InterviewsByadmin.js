802a7354325edc79604178003921ab54562ee056
ali zaman <ali.zaman@bt.pk>
2024-02-23 20:14:34 +0500

    updated

--- controllers/Admin/InterviewsByadmin.js
+++ controllers/Admin/InterviewsByadmin.js
@@ -5,7 +5,7 @@ const respond = require("../../utilis/responseHelper");
 const getScheduleInterviewAdmin = async (req, res, next) => {
   try {
     const {candidateId, jobId} = req.body
-    console.log(candidateId, jobId, "bodyyyyyyyyyyyyyy")
+    console.log(candidateId, jobId, "bodyyyyyyyyyyyy")
     const interviews = await Interview.find({ $and: [{ candidateId: { $gte: candidateId } }, { jobId: jobId }] });
 
     respond(res, { interviews });
